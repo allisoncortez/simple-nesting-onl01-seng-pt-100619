56a7eb3187a6a5b23ce0bdb648838d3e09371bcf
allison cortez <allizetorc@gmail.com>
2019-10-19 04:11:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,6 @@ def adding_matz
      }
 
 programmer_hash[:yukihiro_matsumoto] = []
-
 programmer_hash
     
 end
@@ -106,8 +105,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
+     programmer_hash[:alan_kay]
      
 end
 
