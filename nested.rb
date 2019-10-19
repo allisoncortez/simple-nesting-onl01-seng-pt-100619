6415d7ea8744e08fc1918e1057173f0cf7bf64a8
allison cortez <allizetorc@gmail.com>
2019-10-19 04:17:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,6 @@ def adding_matz
 
 programmer_hash[:yukihiro_matsumoto] = []
 programmer_hash
-    
 end
 
 def changing_alan
@@ -106,9 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     
      programmer_hash
-     
 end
 
 def adding_to_dennis
@@ -127,5 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+     programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
+     programmer_hash
 end
